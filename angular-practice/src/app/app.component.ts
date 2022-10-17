04fa0df95f5a1d7e7eae16faa933fb0eef42d33c
Mateusz <mateusz.grzybowski98@gmail.com>
2022-10-17 11:10:28 +0300

    Created adding member funcionality

--- angular-practice/src/app/app.component.ts
+++ angular-practice/src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-practice';
+  newMemberName = '';
+  members: string[] = [];
+
+  addMember() {
+    this.members.push(this.newMemberName);
+  }
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
 }
